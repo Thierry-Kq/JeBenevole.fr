85735c448355ff4b258d565ce5b045342d01b198
jerem <jerem@LAPTOP>
2021-08-26 12:29:26 +0200

    Association route set up

--- src/Controller/AssociationController.php
+++ src/Controller/AssociationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Associations;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +10,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class AssociationController extends AbstractController
 {
     /**
-     * @Route("/association", name="association")
+     * @Route("/association/modification/{slug}", name="edit_association")
      */
-    public function index(): Response
+    public function edit(string $slug): Response
     {
-        return $this->render('association/index.html.twig', [
+
+        $association = $this->getDoctrine()->getRepository(Associations::class)->find($slug);
+
+        return $this->render('association/edit.html.twig', [
+            'controller_name' => 'AssociationController',
+        ]);
+    }
+
+    /**
+     * @Route("/association/suppression/{slug}", name="delete_association", methods="DELETE")
+     */
+    // public function delete(): Response
+    // {
+    
+    // }
+
+    /**
+     * @Route("/association/{slug}", name="show_association")
+     */
+    public function show(string $slug): Response
+    {
+        $association = $this->getDoctrine()->getRepository(Associations::class)->find($slug);
+
+        return $this->render('association/show.html.twig', [
             'controller_name' => 'AssociationController',
         ]);
     }
