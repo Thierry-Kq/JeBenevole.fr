70797430bc23b9ba916689fcbb552e08ddc9c67a
jerem <jerem@LAPTOP>
2021-08-27 22:36:45 +0200

    minor fixe in form for required

--- src/Form/AssociationType.php
+++ src/Form/AssociationType.php
@@ -44,38 +44,48 @@ class AssociationType extends AbstractType
                 ])
             ->add('fixNumber', null, [
                 'label' => 'association_fixNumber',
+                'required' => false,
                 'constraints' => [new Length(['max' => 10])]
                 ])
             ->add('cellNumber', null, [
                 'label' => 'association_cellNumber',
+                'required' => false,
                 'constraints' => [new Length(['max' => 10])]
                 ])
             ->add('faxNumber', null, [
                 'label' => 'association_faxNumber',
+                'required' => false,
                 'constraints' => [new Length(['max' => 10])]
                 ])
             ->add('description', TextareaType::class, [
                 'label' => 'association_description',
+                'required' => false,
                 ])
             ->add('picture', FileType::class, [
                 'label' => 'association_picture',
+                'required' => false,
                 'data_class' => null])
             ->add('webSite', UrlType::class, [
                 'label' => 'association_website',
+                'required' => false,
                 'constraints' => [new Length(['max' => 200])]])
             ->add('facebook', UrlType::class, [
                 'label' => 'association_facebook',
+                'required' => false,
                 'constraints' => [new Length(['max' => 200])]
                 ])
             ->add('linkedin', UrlType::class, [
                 'label' => 'association_linkedin',
+                'required' => false,
                 'constraints' => [new Length(['max' => 200])]])
             ->add('youtube', UrlType::class, [
                 'label' => 'association_youtube',
+                'required' => false,
                 'constraints' => [new Length(['max' => 200])]
                 ])
             ->add('twitter', UrlType::class, [
                 'label' => 'association_twitter',
+                'required' => false,
                 'constraints' => [new Length(['max' => 200])]
                 ])
         ;
