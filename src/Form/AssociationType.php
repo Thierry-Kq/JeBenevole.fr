b1a64c88e329b3a502b610b718c1bb9f73dea51f
jerem <jerem@LAPTOP>
2021-08-31 22:37:42 +0200

    translation form update

--- src/Form/AssociationType.php
+++ src/Form/AssociationType.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\Url;
 
@@ -21,45 +22,36 @@ class AssociationType extends AbstractType
     {
         $builder
             ->add('name', null, [
-                'label' => 'association_name',
                 'constraints' => [new Length(['max' => 100])]
             ])
             ->add('email', EmailType::class, [
-                'label' => 'association_email',
+                'constraints' => [new Email()]
             ])
             ->add('address', null, [
-                'label' => 'association_address',
                 'constraints' => [new Length(['max' => 100])]
             ])
             ->add('zip', null, [
-                'label' => 'association_zip',
                 'constraints' => [new Length(['max' => 10])],
                 ])
             ->add('city', null, [
-                'label' => 'association_city',
                 'constraints' => [new Length(['max' => 100])]
                 ])
             ->add('fixNumber', null, [
-                'label' => 'association_fixNumber',
                 'required' => false,
                 'constraints' => [new Length(['max' => 10])]
                 ])
             ->add('cellNumber', null, [
-                'label' => 'association_cellNumber',
                 'required' => false,
                 'constraints' => [new Length(['max' => 10])]
                 ])
             ->add('faxNumber', null, [
-                'label' => 'association_faxNumber',
                 'required' => false,
                 'constraints' => [new Length(['max' => 10])]
                 ])
             ->add('description', TextareaType::class, [
-                'label' => 'association_description',
                 'required' => false,
                 ])
             ->add('picture', FileType::class, [
-                'label' => 'association_picture',
                 'required' => false,
                 'constraints' => [
                     new File([
@@ -75,26 +67,21 @@ class AssociationType extends AbstractType
                 ],
                 'data_class' => null])
             ->add('webSite', UrlType::class, [
-                'label' => 'association_website',
                 'required' => false,
                 'constraints' => [new Length(['max' => 200]), new Url()]])
             ->add('facebook', UrlType::class, [
-                'label' => 'association_facebook',
                 'required' => false,
                 'constraints' => [new Length(['max' => 200]), new Url(), new Regex([
                     'pattern' => '/facebook\.com\/[a-zA-Z0-9_]*$/'])]])
             ->add('linkedin', UrlType::class, [
-                'label' => 'association_linkedin',
                 'required' => false,
                 'constraints' => [new Length(['max' => 200]), new Url(), new Regex([
                     'pattern' => '/linkedin\.com\/[a-zA-Z0-9_]*$/'])]])
             ->add('youtube', UrlType::class, [
-                'label' => 'association_youtube',
                 'required' => false,
                 'constraints' => [new Length(['max' => 200]), new Url(), new Regex([
                     'pattern' => '/youtube\.com\/[a-zA-Z0-9_]*$/'])]])
             ->add('twitter', UrlType::class, [
-                'label' => 'association_twitter',
                 'required' => false,
                 'constraints' => [new Length(['max' => 200]), new Url(), new Regex([
                     'pattern' => '/twitter\.com\/[a-zA-Z0-9_]*$/'])]])
